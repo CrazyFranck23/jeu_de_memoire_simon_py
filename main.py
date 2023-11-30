385753de61ed5e3c4c16436ca7ad5400e741934d
Franck NOUTSA FOTSO <fotsofranck237@gmail.com>
2023-11-30 15:05:43 +0100

    Finished version of the memory game

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+import time
+import os
+
+""" Generation d'un nbr aléatoire, un premier nb est généré, puis converti en string ensuite ajouté a la variable 
+random_sequence_str et ainsi de suite jusqu'a obtenir 4 et sortir de la boucle """
+random_sequence_str = ""
+for i in range(4):
+    n = random.randint(0, 9)
+    random_sequence_str += str(n)
+
+
+# Cette fonction permet d'efffacer ce qui est afficher dans le terminal selon le system
+def clear_screen():
+    if os.name == 'posix':  # Systeme Mac ou Linux
+        os.system('clear')
+    else:
+        os.system('cls')  # System Windows
+
+
+# random_sequence_str = "5454"
+score = 0
+good_answer = 1
+
+while good_answer == 1:
+    print("Retenez la sequence")
+    time.sleep(1)
+    print(random_sequence_str)
+    time.sleep(2)
+    clear_screen()
+
+    user_answer = input("Votre réponse : ")
+
+    if user_answer == random_sequence_str:
+        score += 1
+        print("Bonne réponse ! Votre score : ", score)
+        new_number = str(random.randint(0, 9))  # Ici on genere un nouveau nbr
+        random_sequence_str += new_number  # Puis ce nbr est ajouter a notre sequence déjà existante
+        time.sleep(2)
+        clear_screen()
+    else:
+        print("Mauvaise réponse, la sequence était : ", random_sequence_str)
+        print("Votre score final : ", score)
+        good_answer = 0
